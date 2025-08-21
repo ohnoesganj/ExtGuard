66cf5834236512b519dad8537d80c058c51849e8
ohnoesganj <sunho3456@naver.com>
2025-08-21 22:20:59 +0900

    feat: server 구현

--- backend/routes/fixedRoutes.js
+++ backend/routes/fixedRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router;
+const router = express.Router();
 const fixedController = require("../controllers/fixedController");
 
 router.get("/", fixedController.getFixedExtensions);
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const pool = require("./config/db");
+const db = require("./config/db");
+
+const fixedRoutes = require("./routes/fixedRoutes");
+const customRoutes = require("./routes/customRoutes");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/db-test", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT 1 + 1 AS result");
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+app.use("/fixed", fixedRoutes);
+app.use("/custom", customRoutes);
 
 const PORT = 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
