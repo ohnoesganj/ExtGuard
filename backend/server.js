c6ce1fb3f4ee3fcd78912d147eaa1af3691345f4
ohnoesganj <sunho3456@naver.com>
2025-08-23 19:35:03 +0900

    refactor: db 연결 확인

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,6 @@ const db = require("./config/db");
 const fixedRoutes = require("./routes/fixedRoutes");
 const customRoutes = require("./routes/customRoutes");
 
-app.get("/api/test", async (req, res) => {
-  try {
-    const [rows] = await db.query("SELECT 1 + 1 AS result");
-    res.json(rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("DB query error");
-  }
-});
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
