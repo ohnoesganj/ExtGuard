038ee02d58ed52a40b2c0e485d47e6187df5f36f
ohnoesganj <sunho3456@naver.com>
2025-08-23 21:56:39 +0900

    refactor: 경로 수정

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/fixed", fixedRoutes);
+app.use("/api/custom", customRoutes);
+
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
-
-app.use("/api/fixed", fixedRoutes);
-app.use("/api/custom", customRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err);
